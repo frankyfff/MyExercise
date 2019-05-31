013f8d8a8fdb05c5fc45fffbed830e9a0547e143
mamiri <mamiri@PC05160760.cc-college.gecs.loc>
2019-05-31 10:10:35 +0200

    update

--- OCPKlausurFragen/src/fragenConner/Q01.java
+++ OCPKlausurFragen/src/fragenConner/Q01.java
@@ -1,10 +1,32 @@
 package fragenConner;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Q01 {
 
-	public static void main(String[] args) {
-		System.out.println("tmp");
-
+	public static void main(String[] args) throws IOException {
+		Path file = Paths.get("courses.txt");
+		
+		//line n1
+		
 	}
+	
+	/**
+	 * 
+	 * Assume the courses.txtis accessible.
+	 * Which code fragment can be inserted at line n1
+	 * to enable the code to print the content of the courses.txtfile?
+	 * 
+	 * A. List<String> fc = Files.list(file); fc.stream().forEach (s -> System.out.println(s));
+	 * B. Stream<String> fc = Files.readAllLines (file); fc.forEach (s -> System.out.println(s));
+	 * C. List<String> fc = readAllLines(file); fc.stream().forEach (s -> System.out.println(s));
+	 * D. Stream<String> fc = Files.lines (file); fc.forEach (s -> System.out.println(s));
+	 * 
+	 * Correct Answer: D
+	 */
 
 }
---
+++ OCPKlausurFragen/src/fragenConner/Q03.java
@@ -0,0 +1,40 @@
+package fragenConner;
+
+public class Q03 {
+
+/*4*/	void doStuff() throws ArithmeticException, NumberFormatException, Exception {
+/*5*/		if (Math.random() > -1) 
+				throw new Exception ("Try again"); 
+		}
+
+
+	public static void main(String[] args) {
+		
+/*24*/		try {
+/*25*/			doStuff();
+/*26*/		}catch(ArithmeticException | NumberFormatException | Exception e) {
+/*27*/			System.out.println(e.getMessage());
+			}catch(Exception e) {
+/*29*/			System.out.println(e.getMessage());
+/*30*/		}
+	}
+	
+	
+	/**
+	 * Which modification enables the code to print Try again?
+	 * 
+	 * A. Comment the lines 28, 29 and 30.
+	 * 
+	 * B. Replace line 26 with:
+	 * 		} catch (Exception | ArithmeticException | NumberFormatException e) {
+	 * 
+	 * C.Replace line 26 with:
+	 * 		} catch (ArithmeticException | NumberFormatException e) {
+	 * 
+	 * D.Replace line 27 with:    throw e;
+	 * 
+	 * 
+	 * Correct Answer: C
+	 */
+
+}
